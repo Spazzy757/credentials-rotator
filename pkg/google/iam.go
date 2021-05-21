c4ef61eab65eb66aefe45df7f8fb3d728f1eebb4
Spazzy <brendankamp757@gmail.com>
2021-05-21 16:58:29 +0200

    added list and delete keys
    Signed-off-by: Spazzy <brendankamp757@gmail.com>

--- pkg/google/iam.go
+++ pkg/google/iam.go
@@ -8,6 +8,7 @@ import (
 	adminpb "google.golang.org/genproto/googleapis/iam/admin/v1"
 )
 
+//CreateKey creates a new key for a service account
 func CreateKey(
 	ctx context.Context,
 	project string,
@@ -27,4 +28,47 @@ func CreateKey(
 		return nil, err
 	}
 	return resp, nil
+}
+
+//ListKeys returns all the keys under a service account
+func ListKeys(
+	ctx context.Context,
+	project string,
+	serviceAccount string,
+	client *iam.IamClient,
+) (*adminpb.ListServiceAccountKeysResponse, error) {
+	formatted := fmt.Sprintf(
+		"projects/%v/serviceAccounts/%v",
+		project,
+		serviceAccount,
+	)
+	request := &adminpb.ListServiceAccountKeysRequest{
+		Name: formatted,
+	}
+	resp, err := client.ListServiceAccountKeys(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+//DeleteKey deletes a service account key
+func DeleteKey(
+	ctx context.Context,
+	project string,
+	serviceAccount string,
+	key string,
+	client *iam.IamClient,
+) error {
+	formatted := fmt.Sprintf(
+		"projects/%s/serviceAccounts/%s/keys/%s",
+		project,
+		serviceAccount,
+		key,
+	)
+	request := &adminpb.DeleteServiceAccountKeyRequest{
+		Name: formatted,
+	}
+	err := client.DeleteServiceAccountKey(ctx, request)
+	return err
 }
--- pkg/google/iam_test.go
+++ pkg/google/iam_test.go
@@ -8,12 +8,12 @@ import (
 	"log"
 	"net"
 	"os"
-	"reflect"
 	"strings"
 	"testing"
 
 	iam "cloud.google.com/go/iam/admin/apiv1"
 	"github.com/golang/protobuf/ptypes"
+	emptypb "github.com/golang/protobuf/ptypes/empty"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/api/option"
 	adminpb "google.golang.org/genproto/googleapis/iam/admin/v1"
@@ -56,7 +56,37 @@ func (s *mockIamServer) CreateServiceAccountKey(
 	if s.err != nil {
 		return nil, s.err
 	}
-	return &adminpb.ServiceAccountKey{}, nil
+	return s.resps[0].(*adminpb.ServiceAccountKey), nil
+}
+
+func (s *mockIamServer) ListServiceAccountKeys(
+	ctx context.Context,
+	req *adminpb.ListServiceAccountKeysRequest,
+) (*adminpb.ListServiceAccountKeysResponse, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	if xg := md["x-goog-api-client"]; len(xg) == 0 || !strings.Contains(xg[0], "gl-go/") {
+		return nil, fmt.Errorf("x-goog-api-client = %v, expected gl-go key", xg)
+	}
+	s.reqs = append(s.reqs, req)
+	if s.err != nil {
+		return nil, s.err
+	}
+	return s.resps[0].(*adminpb.ListServiceAccountKeysResponse), nil
+}
+
+func (s *mockIamServer) DeleteServiceAccountKey(
+	ctx context.Context,
+	req *adminpb.DeleteServiceAccountKeyRequest,
+) (*emptypb.Empty, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	if xg := md["x-goog-api-client"]; len(xg) == 0 || !strings.Contains(xg[0], "gl-go/") {
+		return nil, fmt.Errorf("x-goog-api-client = %v, expected gl-go key", xg)
+	}
+	s.reqs = append(s.reqs, req)
+	if s.err != nil {
+		return nil, s.err
+	}
+	return s.resps[0].(*emptypb.Empty), nil
 }
 
 // clientOpt is the option tests should use to connect to the test server.
@@ -94,6 +124,10 @@ func TestGetKeys(t *testing.T) {
 		ctx := context.Background()
 		project := "project-1"
 		serviceAccount := "test@example.com"
+		expectedResponse := &adminpb.ServiceAccountKey{}
+		mockIam.err = nil
+		mockIam.reqs = nil
+		mockIam.resps = append(mockIam.resps[:0], expectedResponse)
 
 		client, err := iam.NewIamClient(ctx, clientOpt)
 
@@ -101,9 +135,8 @@ func TestGetKeys(t *testing.T) {
 
 		key, err := CreateKey(ctx, project, serviceAccount, client)
 
-		expectedKey := reflect.TypeOf(&adminpb.ServiceAccountKey{})
 		assertions.NoError(err)
-		assertions.Equal(expectedKey, reflect.TypeOf(key))
+		assertions.Equal(expectedResponse, key)
 	})
 	t.Run("Test Get Keys Returned Error", func(t *testing.T) {
 		errCode := codes.PermissionDenied
@@ -119,6 +152,81 @@ func TestGetKeys(t *testing.T) {
 		assertions.NoError(err)
 
 		_, err = CreateKey(ctx, project, serviceAccount, client)
+		assertions.Error(err)
+	})
+}
+
+func TestListKeys(t *testing.T) {
+	t.Run("Test List Keys", func(t *testing.T) {
+		assertions := require.New(t)
+		ctx := context.Background()
+		project := "project-1"
+		serviceAccount := "test@example.com"
+
+		client, err := iam.NewIamClient(ctx, clientOpt)
+
+		assertions.NoError(err)
+		expectedResponse := &adminpb.ListServiceAccountKeysResponse{}
+		mockIam.err = nil
+		mockIam.reqs = nil
+		mockIam.resps = append(mockIam.resps[:0], expectedResponse)
+
+		key, err := ListKeys(ctx, project, serviceAccount, client)
+
+		assertions.NoError(err)
+		assertions.Equal(expectedResponse, key)
+	})
+	t.Run("Test List Keys Returned Error", func(t *testing.T) {
+		errCode := codes.PermissionDenied
+		mockIam.err = gstatus.Error(errCode, "test error")
+
+		assertions := require.New(t)
+		ctx := context.Background()
+		project := "project-1"
+		serviceAccount := "test@example.com"
+
+		client, err := iam.NewIamClient(ctx, clientOpt)
+
+		assertions.NoError(err)
+
+		_, err = ListKeys(ctx, project, serviceAccount, client)
+		assertions.Error(err)
+	})
+}
+
+func TestDeleteKey(t *testing.T) {
+	t.Run("Test Delete Key", func(t *testing.T) {
+		assertions := require.New(t)
+		ctx := context.Background()
+		project := "project-1"
+		serviceAccount := "test@example.com"
+		key := "key-name"
+		client, err := iam.NewIamClient(ctx, clientOpt)
+
+		assertions.NoError(err)
+		expectedResponse := &emptypb.Empty{}
+		mockIam.err = nil
+		mockIam.reqs = nil
+		mockIam.resps = append(mockIam.resps[:0], expectedResponse)
+
+		err = DeleteKey(ctx, project, serviceAccount, key, client)
+
+		assertions.NoError(err)
+	})
+	t.Run("Test Delete Key Returned Error", func(t *testing.T) {
+		errCode := codes.PermissionDenied
+		mockIam.err = gstatus.Error(errCode, "test error")
+
+		assertions := require.New(t)
+		ctx := context.Background()
+		project := "project-1"
+		serviceAccount := "test@example.com"
+		key := "key-name"
+		client, err := iam.NewIamClient(ctx, clientOpt)
+
+		assertions.NoError(err)
+
+		err = DeleteKey(ctx, project, serviceAccount, key, client)
 		assertions.Error(err)
 	})
 }
